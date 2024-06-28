8e70befe431c6d91e23fa9aa2c20f45e2db6f500
Welson Douglas Oliveira Gomes <92097961+WelsonGomes@users.noreply.github.com>
2024-06-28 14:53:05 -0300

    atualização

--- src/server.ts
+++ src/server.ts
@@ -89,106 +89,49 @@ app.get('/Venda', async (req, res, next) => {
 });
 
 app.post('/Venda', async (req, res, next) => {
-    try {
-        // Primeiro, faça a requisição POST para registrar a venda
-        const vendaResp = await axios.post(
-          "http://localhost:3002/Venda",
-          {
-            produtoid: req.body.produtoid,
-            qtdade: req.body.qtdade,
-            valorproduto: req.body.valorproduto
-          },
-          {
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json;charset=UTF-8'
-            }
-          }
-        );
-    
-        let result = false;
-        if (vendaResp) {
-          console.log('Venda registrada com sucesso');
-          // Depois, atualize o estoque do produto
-          const produtoResp = await axios.put(
-            "http://localhost:3001/Produto",
-            {}, // Corpo vazio
-            {
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-              },
-              params: {
-                id: req.body.produtoid,
-                qtd: req.body.qtdade,
-                op: 'subtrair'
-              }
-            }
-          );
-    
-          console.log('Resposta da atualização do produto:', produtoResp.data);
-          if (produtoResp.data.success) {
-            result = true;
-          }
+  try {
+    const vendaResp = await axios.post(
+      "http://localhost:3002/Venda",
+      {
+        produtoid: req.body.produtoid,
+        qtdade: req.body.qtdade,
+        valorproduto: req.body.valorproduto
+      },
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8'
         }
-        console.log(vendaResp.data)
-        return res.status(200).json({ msg: vendaResp.data.msg });
-      } catch (error) {
-        console.error('Erro ao processar venda:', error);
-        return res.status(500).json({ msg: 'Erro interno ao processar a solicitação.' });
-    }
+      }
+    );
+    console.log(vendaResp.data);
+    return res.status(200).json({ msg: vendaResp.data.msg });
+  } catch (error) {
+    console.error('Erro ao processar venda:', error);
+    return res.status(500).json({ msg: 'Erro interno ao processar a solicitação.' });
+  }
 });
 
 app.delete('/Venda', async (req, res, next) => {
-    try {
-        // Primeiro, faça a requisição POST para registrar a venda
-        const vendaResp = await axios.delete(
-            "http://localhost:3002/Venda",
-            {
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-              },
-              params: {
-                id: req.query.id,
-              }
-            }
-          );
-
-        let result = false;
-        if (vendaResp) {
-            const produtoid = vendaResp.data.produtoid;
-            const qtd = vendaResp.data.qtd;
-            console.log(produtoid, qtd)
-          console.log('Venda cancelada com sucesso');
-          // Depois, atualize o estoque do produto
-          const produtoResp = await axios.put(
-            "http://localhost:3001/Produto",
-            {}, // Corpo vazio
-            {
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-              },
-              params: {
-                id: produtoid,
-                qtd: qtd,
-                op: 'soma'
-              }
-            }
-          );
-    
-          console.log('Resposta da atualização do produto:', produtoResp.data);
-          if (produtoResp.data.success) {
-            result = true;
-          }
+  try {
+    const vendaResp = await axios.delete(
+      "http://localhost:3002/Venda",
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+        params: {
+          id: req.query.id,
         }
-        console.log(vendaResp.data)
-        return res.status(200).json({ msg: vendaResp.data.msg });
-      } catch (error) {
-        console.error('Erro ao processar venda:', error);
-        return res.status(500).json({ msg: 'Erro interno ao processar a solicitação.' });
-    }
+      }
+    );
+    console.log(vendaResp.data);
+    return res.status(200).json({ msg: vendaResp.data.msg });
+  } catch (error) {
+    console.error('Erro ao processar venda:', error);
+    return res.status(500).json({ msg: 'Erro interno ao processar a solicitação.' });
+  }
 });
 
 app.listen(port, () => {
